c69cc8d3bbaa3cf1d429702211f4e140e1c14e06
Volodymyr Vladykin <vova@pocketguard.com>
2025-01-11 21:57:27 +0200

    task 3 complete

--- js/task-1.js
+++ js/task-1.js
@@ -6,3 +6,4 @@ function makeTransaction(quantity, pricePerDroid) {
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log("----------------"); // to have nice padding between tasks output in console
--- js/task-2.js
+++ js/task-2.js
@@ -6,3 +6,4 @@ function getShippingMessage(country, price, deliveryFee) {
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
 console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log("----------------"); // to have nice padding between tasks output in console
